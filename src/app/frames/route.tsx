5138bb7a46f8eee2770954693ea614176738ece1
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-04-18 01:13:42 +0200

    fix: update initial frame

--- src/app/frames/route.tsx
+++ src/app/frames/route.tsx
@@ -1,15 +1,26 @@
 import { Button } from "frames.js/next";
 import { frames } from "./frames";
+import { getRandomNounSeed } from "@nouns/assets";
+import { nounImageUrl } from "../utils";
 
 const frameHandler = frames(async (ctx) => {
+  const seed = getRandomNounSeed();
+
+  const nounUrl = nounImageUrl(seed);
+
   return {
-    image: <div>Hello</div>,
+    image: (
+      <div tw="flex relative">
+        <img src={nounUrl} />
+        <div tw="absolute w-full justify-center mt-2">Nouns Playground</div>
+      </div>
+    ),
     imageOptions: {
       aspectRatio: "1:1",
     },
     buttons: [
       <Button action="post" target={"/builder"}>
-        Start
+        Customize
       </Button>,
     ],
   };
